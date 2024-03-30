24710c4390b95065ea4effbb5a8a507a29a28be6
AngelinaPuh <angelinapuhnenkova@gmail.com>
2024-03-30 10:52:55 +0300

    add

--- laba_6/lab_06.py
+++ laba_6/lab_06.py
@@ -2,7 +2,7 @@ import turtle
 turtle.shape("turtle")
 turtle.speed(100)
 turtle.color("red")
-for i in range(20):
- turtle.forward(150)
+for i in range(20): 
+ turtle.forward(150) 
  turtle.left(126)
 turtle.exitonclick()
