5b4639c1de3579117aa9c4f14c296faebfc9d10c
AngelinaPuh <angelinapuhnenkova@gmail.com>
2024-03-30 10:59:37 +0300

    add

--- laba_8/dz_8.py
+++ laba_8/dz_8.py
@@ -3,4 +3,4 @@ string = input("Введите строку\n").lower()
 wodr = input("Введите слово\n").lower()
 if string.find(wodr) != -1:
     x = string.find(wodr)
-    print(f"нашли слово! на позиции {x}") 
+    print(f"нашли слово! на позиции {x}")  
